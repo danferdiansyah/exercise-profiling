7de2ddb8294ca9b40a7c88c5b63579ee22676879
danferdiansyah <danferdianstyle@gmail.com>
2025-03-13 15:06:55 +0700

    refactor: optimizing getAllStudentsWithSourses() by refactoring StudentCourseRepository

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentCourseRepository.java
@@ -13,4 +13,10 @@ import java.util.List;
 @Repository
 public interface StudentCourseRepository extends JpaRepository<StudentCourse, Long> {
     List<StudentCourse> findByStudentId(Long studentId);
+    /**
+     * Find all student courses where the student ID is in the provided list
+     * @param studentIds List of student IDs to search for
+     * @return List of student courses that match the criteria
+     */
+    List<StudentCourse> findByStudentIdIn(List<Long> studentIds);
 }
